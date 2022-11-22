dbb2f7ce3056b7a00fd2d7f9aefb8de864be1a21
Jamesson Faria <jamessonjr@gmail.com>
2022-11-22 17:33:12 -0300

    Trabalhando com o expanded

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -3,49 +3,31 @@ import 'package:flutter/material.dart';
 class TodoListPage extends StatelessWidget {
   TodoListPage({Key? key}) : super(key: key);
 
-  final TextEditingController emailController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
             children: [
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  labelText: "Email",
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Adicione uma tarefa",
+                      hintText: "Exemplo estudar flutter"),
                 ),
-                onChanged: onChanged,
-                onSubmitted: onSubmitted,
               ),
+              SizedBox(width: 8,),
               ElevatedButton(
-                onPressed: login,
-                child: Text("Entrar"),
-              )
+                onPressed: () {},
+                child: Text("+"),
+              ),
             ],
           ),
         ),
       ),
     );
   }
-
-  void login() {
-    String text = emailController.text;
-    print(text);
-    emailController.clear();
-    emailController.text = "xxx --- xxx";
-  }
-
-  void onChanged(String text) {
-    //print(text);
-  }
-
-  void onSubmitted(String text) {
-    print(text);
-  }
-
 }
