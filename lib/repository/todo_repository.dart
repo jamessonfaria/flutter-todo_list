86bb68b11ba2b8da62d77c8e0face396b235b92d
Jamesson Faria <jamessonjr@gmail.com>
2022-12-03 11:46:16 -0300

    Lendo lista de tarefas do shared preference

--- lib/repository/todo_repository.dart
+++ lib/repository/todo_repository.dart
@@ -3,16 +3,21 @@ import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_list/models/todo.dart';
 
-class TodoRepository {
+const todoListKey = "todo_list";
 
-  TodoRepository() {
-    SharedPreferences.getInstance().then((value) => sharedPreferences = value);
-  }
+class TodoRepository {
 
   late SharedPreferences sharedPreferences;
 
+  Future<List<Todo>> getTodoList() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    final String jsonString = sharedPreferences.getString(todoListKey) ?? '[]';
+    final List jsonDecoded = json.decode(jsonString) as List;
+    return jsonDecoded.map((e) => Todo.fromJson(e)).toList();
+  }
+
   void saveTodoList(List<Todo> todos) {
     final String jsonString = json.encode(todos);
-    sharedPreferences.setString("todo_list", jsonString);
+    sharedPreferences.setString(todoListKey, jsonString);
   }
 }
